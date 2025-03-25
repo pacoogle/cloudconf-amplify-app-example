06d11d6aa1dd86cb05814348ddc5f6b6c435ef80
Pasquale Ragozzino <pasquale.ragozzino89@gmail.com>
2025-03-25 16:32:45 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,54 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { Trash2 } from "lucide-react";
 
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+    useEffect(() => {
+        client.models.Todo.observeQuery().subscribe({
+            next: (data) => setTodos([...data.items]),
+        });
+    }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+    function createTodo() {
+        const content = window.prompt("Todo content");
+        if (content) {
+            client.models.Todo.create({ content });
+        }
+    }
 
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+    function deleteTodo(id: string) {
+        client.models.Todo.delete({ id });
+    }
+
+    return (
+        <main>
+            <h1>CloudConf 2025</h1>
+            <button onClick={createTodo}>Aggiungi</button>
+            <ul>
+                {todos.map((todo) => (
+                    <li
+                        key={todo.id}
+                        style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}
+                    >
+                        <span>{todo.content}</span>
+                        <Trash2
+                            style={{ cursor: "pointer" }}
+                            size={18}
+                            onClick={() => deleteTodo(todo.id)}
+                        />
+                    </li>
+                ))}
+            </ul>
+            <div>
+                <br />
+            </div>
+        </main>
+    );
 }
 
 export default App;
